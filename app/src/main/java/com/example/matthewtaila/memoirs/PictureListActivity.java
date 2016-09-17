7335b7e10e72f2469202082c4f89860b7ea1cf27
MTailaCodes <mtailacodes@gmail.com>
2016-09-17 16:31:20 -0400

    Created Activities and XML layout files for the following: 	1. LoginActivity 	2. AlbumListActivity 	3. PictureListActivity 	4. CreateAlbumActivity

---
+++ app/src/main/java/com/example/matthewtaila/memoirs/PictureListActivity.java
@@ -0,0 +1,13 @@
+package com.example.matthewtaila.memoirs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PictureListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_list);
+    }
+}
