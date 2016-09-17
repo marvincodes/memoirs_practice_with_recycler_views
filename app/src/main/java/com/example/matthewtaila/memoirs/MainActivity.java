d276936b0c6a4a2503b9ce112d7e7f32ef8d4b93
Marvin Albazi <marvin_al-bazi@hotmail.com>
2016-09-18 00:02:15 +0100

    Updated Main Activity logic: 	1. Referenced views 	2. Activity navigation logic

--- app/src/main/java/com/example/matthewtaila/memoirs/MainActivity.java
+++ app/src/main/java/com/example/matthewtaila/memoirs/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.matthewtaila.memoirs;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +13,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button albumListActivity;
     Button pictureListActivity;
     Button createAlbumActivity;
+    Button pictureActivity;
+    Button editAlbumActivity;
+    Button editPictureActivity;
 
 
     @Override
@@ -27,22 +29,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // - Navigation buttons
         logInActivity = (Button)findViewById(R.id.btn_loginActivity);
-        albumListActivity = (Button)findViewById(R.id.btn_albumListActivty);
-        pictureListActivity = (Button)findViewById(R.id.btn_pictureListActivty);
+        albumListActivity = (Button)findViewById(R.id.btn_albumListActivity);
+        pictureListActivity = (Button)findViewById(R.id.btn_pictureListActivity);
         createAlbumActivity = (Button)findViewById(R.id.btn_createAlbumActivity);
+        pictureActivity = (Button)findViewById(R.id.btn_pictureActivity);
+        editAlbumActivity = (Button)findViewById(R.id.btn_editAlbumActivity);
+        editPictureActivity = (Button)findViewById(R.id.btn_editPictureActivity);
 
         // - onClickListener
         logInActivity.setOnClickListener(this);
         albumListActivity.setOnClickListener(this);
         pictureListActivity.setOnClickListener(this);
         createAlbumActivity.setOnClickListener(this);
+        pictureActivity.setOnClickListener(this);
+        editAlbumActivity.setOnClickListener(this);
+        editPictureActivity.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         Intent intent;
         switch (view.getId()){
-            case R.id.btn_albumListActivty:
+            case R.id.btn_albumListActivity:
                 intent = new Intent(this, AlbumListActivity.class);
                 startActivity(intent);
                 break;
@@ -50,12 +58,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 intent = new Intent(this, LoginActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.btn_pictureListActivty:
+            case R.id.btn_pictureListActivity:
                 intent = new Intent(this, PictureListActivity.class);
                 startActivity(intent);
                 break;
             case R.id.btn_createAlbumActivity:
                 intent = new Intent(this, CreateAlbumActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_pictureActivity:
+                intent = new Intent(this,PictureActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_editAlbumActivity:
+                intent = new Intent(this,EditAlbumActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_editPictureActivity:
+                intent = new Intent(this,EditPictureActivity.class);
                 startActivity(intent);
                 break;
         }
