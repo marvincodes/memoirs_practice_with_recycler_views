1b5538da7547029ee9d51f22d9a142c8d7624c52
MTailaCodes <mtailacodes@gmail.com>
2016-09-17 16:34:49 -0400

    Applied logic for activity navigation from MainActivity: 	1. ReferenceView method - referenced views 	2. Implemented onClickListener (override onClick method) 	3. Implemented switch case method to navigate upon button click

--- app/src/main/java/com/example/matthewtaila/memoirs/MainActivity.java
+++ app/src/main/java/com/example/matthewtaila/memoirs/MainActivity.java
@@ -1,15 +1,63 @@
 package com.example.matthewtaila.memoirs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    // - UI
+    Button logInActivity;
+    Button albumListActivity;
+    Button pictureListActivity;
+    Button createAlbumActivity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Main Activity", "Test log");
+        referenceViews();
+    }
+
+    private void referenceViews() {
+
+        // - Navigation buttons
+        logInActivity = (Button)findViewById(R.id.btn_loginActivity);
+        albumListActivity = (Button)findViewById(R.id.btn_albumListActivty);
+        pictureListActivity = (Button)findViewById(R.id.btn_pictureListActivty);
+        createAlbumActivity = (Button)findViewById(R.id.btn_createAlbumActivity);
+
+        // - onClickListener
+        logInActivity.setOnClickListener(this);
+        albumListActivity.setOnClickListener(this);
+        pictureListActivity.setOnClickListener(this);
+        createAlbumActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()){
+            case R.id.btn_albumListActivty:
+                intent = new Intent(this, AlbumListActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_loginActivity:
+                intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_pictureListActivty:
+                intent = new Intent(this, PictureListActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_createAlbumActivity:
+                intent = new Intent(this, CreateAlbumActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
