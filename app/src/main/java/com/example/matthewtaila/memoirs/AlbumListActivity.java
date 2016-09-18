cc974aed1c17969064f5a7b732016f571a2e6553
MTailaCodes <mtailacodes@gmail.com>
2016-09-18 15:57:37 -0400

    Created stub data for ArrayList<Album> and set up recyclerView and Adapter Logic

--- app/src/main/java/com/example/matthewtaila/memoirs/AlbumListActivity.java
+++ app/src/main/java/com/example/matthewtaila/memoirs/AlbumListActivity.java
@@ -2,12 +2,46 @@ package com.example.matthewtaila.memoirs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.matthewtaila.memoirs.Model.Album;
+
+import java.util.ArrayList;
 
 public class AlbumListActivity extends AppCompatActivity {
+
+    // - Logic
+    ArrayList<Album> albumArrayList;
+    AlbumAdapter albumAdapter;
+
+    // - UI
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album_list);
+        recyclerView = (RecyclerView)findViewById(R.id.rv_albumList);
+        albumArrayList = new ArrayList<>();
+        createStubAlbumData();
+        setupAlbumRecyclerView();
     }
+
+    private void setupAlbumRecyclerView() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        albumAdapter = new AlbumAdapter(albumArrayList, this);
+        recyclerView.setAdapter(albumAdapter);
+    }
+
+    private void createStubAlbumData() {
+        Album album;
+        for (int i = 0; i < 10; i++){
+            album = new Album();
+            album.setTitle("Album number: " + i);
+            albumArrayList.add(album);
+        }
+    }
+
+
 }
