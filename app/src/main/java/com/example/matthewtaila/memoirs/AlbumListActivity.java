fe8424946dd29a8876c096d0a10bc1c6d1bed638
MTailaCodes <mtailacodes@gmail.com>
2016-09-20 19:32:16 -0400

    no message

--- app/src/main/java/com/example/matthewtaila/memoirs/AlbumListActivity.java
+++ app/src/main/java/com/example/matthewtaila/memoirs/AlbumListActivity.java
@@ -1,15 +1,18 @@
 package com.example.matthewtaila.memoirs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.matthewtaila.memoirs.Model.Album;
 
 import java.util.ArrayList;
 
-public class AlbumListActivity extends AppCompatActivity {
+public class AlbumListActivity extends AppCompatActivity implements View.OnClickListener {
 
     // - Logic
     ArrayList<Album> albumArrayList;
@@ -17,11 +20,14 @@ public class AlbumListActivity extends AppCompatActivity {
 
     // - UI
     RecyclerView recyclerView;
+    Button createAlbumButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album_list);
+        createAlbumButton = (Button)findViewById(R.id.btn_createAlbum);
+        createAlbumButton.setOnClickListener(this);
         recyclerView = (RecyclerView)findViewById(R.id.rv_albumList);
         albumArrayList = new ArrayList<>();
         createStubAlbumData();
@@ -44,4 +50,13 @@ public class AlbumListActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_createAlbum:
+                Intent intent = new Intent(this, CreateAlbumActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
 }
