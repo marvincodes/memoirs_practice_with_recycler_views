e99551dda3b1d05be33b50182433dc38247faeda
Marvin Albazi <marvin_al-bazi@hotmail.com>
2016-09-21 03:14:50 +0100

    Create album activity

--- app/src/main/java/com/example/matthewtaila/memoirs/CreateAlbumActivity.java
+++ app/src/main/java/com/example/matthewtaila/memoirs/CreateAlbumActivity.java
@@ -2,12 +2,41 @@ package com.example.matthewtaila.memoirs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class CreateAlbumActivity extends AppCompatActivity {
+import com.example.matthewtaila.memoirs.Model.Album;
+
+public class CreateAlbumActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // UI
+    EditText albumTitleStore;
+    Button buttonTitleStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_album);
+        albumTitleStore = (EditText) findViewById(R.id.et_createAlbumTitle);
+        buttonTitleStore = (Button) findViewById(R.id.btn_storeAlbumTitle);
+        buttonTitleStore.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_storeAlbumTitle:
+                storeAlbum();
+                break;
+        }
+    }
+
+    private void storeAlbum() {
+        String albumTitle = albumTitleStore.getText().toString();
+        Album album = new Album();
+        album.setTitle(albumTitle);
+        Log.i("Test input", album.getTitle());
     }
 }
